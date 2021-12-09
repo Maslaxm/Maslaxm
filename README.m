f97f94b4a462f6429fe61936f3cec25f54adaf4b
Maslaxm <95336619+Maslaxm@users.noreply.github.com>
2021-12-09 22:19:21 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="true" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div id="liveAlertPlaceholder"></div>
+  <button type="button" class="btn btn-primary"
+  <div class="card-body">
+    <h5 class="card-title">xaruunta adeegista</h5>
+    <p class="card-text">wixii caawinad .</p>
+    <a href="#" class="btn btn-primary">ku soco si lagoo caawiyo</a>
+    <div id="liveAlertPlaceholder"></div>
+    <button type="button" class="btn btn-primary"
+    
+  </div>
+      <div class="bg-success p-2 text-white"></div>
+      <div class="bg-success p-2" style="--bs-bg-opacity: .5;">
+</div>
+    <title>baaciye online!</title>
+<div class="card" style="width: 18rem;">
+  <img src="Maslax logo.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading"></h4>
+      <p>.</p>
+      <hr>
+      <p class="mb-0"/p>
+    </div>
+    <h5 class="card-title">Eng.maslah</h5>
+    <p class="card-text">eng.maslah waa software developer iyo ap develop iyo game developer.</p>
+    <a href="#" class="btn btn-primary">sii fiiri xogtan oodheri ah</a>
+    <div id="liveAlertPlaceholder"></div>
+    <button type="button" class="btn btn-primary"
+    <div id="liveAlertPlaceholder"></div>
+    <button type="button" class="btn btn-primary"
+    <div id="liveAlertPlaceholder"></div>
+    <button type="button" class="btn btn-primary"
+  </div>
+  <div class="asc" tabindex="-1">
+    <div class="wcs">
+      <div class="soo dhowaaw">
+        <div class="waan dhowahay">
+          <h5 class="eng maslah">baaciye</h5>
+          <button type="soo dejiso" class="btn-close" data-bs-dismiss="modal" aria-label="adeeg hufan"></button>
+          <div class="alert alert-primary" role="alert">
+            
+        </div>
+        <div class="modal-body">
+          <p>si wanaagsan u adeego.</p>
+        </div>
+        <div class="baaciye online">
+          <button type="hada dalbo" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="helitaan deg degah" class="btn btn-primary">matkale seef gareyso</button>
+          <div class="alert alert-primary" role="alert">
+            
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </head>
+  <body>
+    <h1>adeeg.hufan!</h1>
+    <div class="bg-success p-2 text-white"></div>
+    <div class="bg-success p-2" style="--bs-bg-opacity: .5;">
+      <div class="input-group">
+        <span class="input-group-text">First and last name</span>
+        <input type="text" aria-label="First name" class="form-control">
+        <input type="text" aria-label="Last name" class="form-control">
+      </div>
+      <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupSelect01">dooro</label>
+        <select class="form-select" id="inputGroupSelect01">
+          <option select>dooro gobolkada</option>
+          <option value="1">bakool</option>
+          <option value="2">baay</option>
+          <option value="3">shabeelada hoose</option>
+        </select>
+      </div>
+      <div class="mb-3 row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">iimelkaada</label>
+        <div class="col-sm-10">
+          <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@tusaale.com">
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">biinkaada</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword">
+        </div>
+      </div>
+      <title>baaciye online!</title>
+      <div class="card" style="width: 18rem;">
+        <img src="Eng maslah.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading"></h4>
+            <p>.</p>
+            <hr>
+            <p class="mb-0" /p>
+          </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+5522102715https:
